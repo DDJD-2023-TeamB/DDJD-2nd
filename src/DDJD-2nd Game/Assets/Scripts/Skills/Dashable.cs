118517b5624c657756f346bf97dba29414d78d30
BrunoRosendo <brunomrosendo@hotmail.com>
2023-04-22 18:52:06 +0100

    Dashing in all directions

--- src/DDJD-2nd Game/Assets/Scripts/Skills/Dashable.cs
+++ src/DDJD-2nd Game/Assets/Scripts/Skills/Dashable.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Dashable : MonoBehaviour
 {
-    Rigidbody _rigidbody;
+    private Rigidbody _rigidbody;
+    private PlayerInputReceiver _inputReceiver;
+
+    [SerializeField]
+    private Transform _playerCam;
+
+    [SerializeField]
+    private bool allowAllDirections = true;
+
+    [SerializeField]
+    private bool disableGravity = false;
+
+    [SerializeField]
+    private bool resetVelocity = true;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _inputReceiver = GetComponent<PlayerInputReceiver>();
     }
 
     public void DashWithSkill(DashSkill dashSkill)
@@ -24,8 +38,45 @@ public class Dashable : MonoBehaviour
 
     public void Dash(DashStats stats)
     {
-        // TODO: Improve movement after dash logic is implemented
-        Vector3 force = transform.forward * stats.Force;
+        Transform forwardTransform;
+        if (allowAllDirections)
+            forwardTransform = _playerCam;
+        else
+            forwardTransform = transform;
+
+        Vector3 direction = GetDirection(forwardTransform);
+        Vector3 force = direction * stats.Force;
+
+        if (disableGravity)
+            _rigidbody.useGravity = false;
+        if (resetVelocity)
+            _rigidbody.velocity = Vector3.zero;
+
         _rigidbody.AddForce(force, ForceMode.Impulse);
+
+        Invoke(nameof(ResetDash), stats.Duration);
+    }
+
+    private void ResetDash()
+    {
+        if (disableGravity)
+            _rigidbody.useGravity = true;
+    }
+
+    private Vector3 GetDirection(Transform forwardTransform)
+    {
+        Vector2 moveInput = _inputReceiver.MoveInput;
+        Vector3 direction = Vector3.zero;
+        if (allowAllDirections && moveInput != Vector2.zero)
+        {
+            direction =
+                forwardTransform.forward * moveInput.y + forwardTransform.right * moveInput.x;
+        }
+        else
+        {
+            direction = forwardTransform.forward;
+        }
+
+        return direction.normalized;
     }
 }
