6f228f03dfcbb85b83c2b7b77dd0d8ee2336c626
BrunoRosendo <brunomrosendo@hotmail.com>
2023-05-02 01:00:10 +0100

    Fixed arc not being set at right position

--- src/DDJD-2nd Game/Assets/Scripts/Skills/Spawn/Electricity/ElectricArcCoilComponent.cs
+++ src/DDJD-2nd Game/Assets/Scripts/Skills/Spawn/Electricity/ElectricArcCoilComponent.cs
@@ -9,11 +9,6 @@ public class ElectricArcCoilComponent : SpawnSkillComponent
 
     public override void Spawn()
     {
-        Transform arcPos1 = _arcPrefab.transform.Find("Pos1");
-        Transform arcPos2 = _arcPrefab.transform.Find("Pos2");
-        Transform arcPos3 = _arcPrefab.transform.Find("Pos3");
-        Transform arcPos4 = _arcPrefab.transform.Find("Pos4");
-
         ObjectSpawner spawner = _caster.GetComponent<ObjectSpawner>();
         List<GameObject> spawnedObjects = spawner.SpawnedObjects;
         List<GameObject> coils = spawnedObjects.FindAll(
@@ -23,25 +18,19 @@ public class ElectricArcCoilComponent : SpawnSkillComponent
             return;
 
         GameObject lastCoil = coils[coils.Count - 2];
+
+        Vector3 arcPosition = (lastCoil.transform.position + transform.position) / 2;
+        GameObject arc = Instantiate(_arcPrefab, arcPosition, Quaternion.identity);
+
+        Transform arcPos1 = arc.transform.Find("Pos1");
+        Transform arcPos2 = arc.transform.Find("Pos2");
+        Transform arcPos3 = arc.transform.Find("Pos3");
+        Transform arcPos4 = arc.transform.Find("Pos4");
         arcPos1.position = lastCoil.transform.position;
         arcPos4.position = transform.position;
         arcPos2.position = 2 * arcPos1.position / 3 + arcPos4.position / 3;
         arcPos3.position = arcPos1.position / 3 + 2 * arcPos4.position / 3;
 
-        Vector3 arcPosition = (arcPos1.position + arcPos4.position) / 2;
-        Debug.Log(
-            "pos1"
-                + arcPos1.position
-                + " pos2"
-                + arcPos2.position
-                + " pos3"
-                + arcPos3.position
-                + " pos4"
-                + arcPos4.position
-                + " arcPos"
-                + arcPosition
-        );
-        GameObject arc = Instantiate(_arcPrefab, arcPosition, Quaternion.identity);
         float arcLife = Mathf.Min(spawner.GetObjectLifeTime(lastCoil), _skill.SpawnStats.Duration);
         Destroy(arc, arcLife);
     }
