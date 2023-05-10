3943323345cc148d5d38b3f71a1d26f05a5af693
Bruno Gomes <bruno.camgomes@gmail.com>
2023-05-10 16:43:45 +0100

    Update wronge change on lightningComponent

--- src/DDJD-2nd Game/Assets/Scripts/Skills/Dash/LightningComponent.cs
+++ src/DDJD-2nd Game/Assets/Scripts/Skills/Dash/LightningComponent.cs
@@ -31,6 +31,13 @@ public class LightningComponent : DashComponent
     protected override void OnImpact(Collider other, float multiplier = 1)
     {
         base.OnImpact(other, multiplier);
+        Damage(
+            other.gameObject,
+            (int)(_skillStats.Damage * multiplier),
+            (int)_skillStats.ForceWithDamage(),
+            other.ClosestPoint(_caster.transform.position),
+            _caster.transform.forward
+        );
     }
 
     private void OnDestroy()
