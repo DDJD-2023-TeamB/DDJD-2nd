6e8f38c6369dc4d2107e25090825e08852899044
monkin77 <monkinsmurf@gmail.com>
2023-05-18 20:29:05 +0100

    Add: ChunksHandler Loads/Unloads according to distance to Player

--- src/DDJD-2nd Game/Assets/Scripts/Environment/SpawnChunks.cs
+++ src/DDJD-2nd Game/Assets/Scripts/Environment/SpawnChunks.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class SpawnChunks : MonoBehaviour
 {
     [SerializeField] private ChunkLoader[] _chunkLoaders;
+    [SerializeField] private Transform _playerTransform;
+
+    // Take into account the distance to the Chunk's Center (ChunkLoaders position is at the center)
+    static float MAX_PLAYER_DISTANCE = (50 * Mathf.Sqrt(2)) + 70;
+
+    Vector2 parseXZ(Vector3 vector3)
+    {
+        return new Vector2(vector3.x, vector3.z);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        // Iterate _chunkloaders
+        for (int i = 0; i < this._chunkLoaders.Length; i++)
         {
-            if (this._chunkLoaders.Length > 0)
+            // Check if the player is close enough to the chunk loader
+            if (Vector2.Distance(parseXZ(this._playerTransform.position), parseXZ(this._chunkLoaders[i].transform.position)) < MAX_PLAYER_DISTANCE)
             {
-                this._chunkLoaders[0].LoadChunk();
+                this._chunkLoaders[i].LoadChunk();
             }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            if (this._chunkLoaders.Length > 0)
+            else
             {
-                this._chunkLoaders[0].UnloadChunk();
+                this._chunkLoaders[i].UnloadChunk();
             }
         }
     }
