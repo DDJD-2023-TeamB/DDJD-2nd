58cb9f7a173f3a52078a1413708f3c0ff357d159
Bruno Gomes <bruno.camgomes@gmail.com>
2023-06-06 21:47:59 +0100

    Camera fix

--- src/DDJD-2nd Game/Assets/Scripts/Player/CameraController.cs
+++ src/DDJD-2nd Game/Assets/Scripts/Player/CameraController.cs
@@ -22,10 +22,10 @@ public class CameraController : MonoBehaviour
     private float _cameraRotationSpeed = 20f;
 
     [SerializeField]
-    private float _minAngle = 30f;
+    private float _maxAngle = 75.0f;
 
     [SerializeField]
-    private float _maxAngle = 330f;
+    private float _minAngle = -45.0f;
     public CinemachineVirtualCamera ActiveCamera
     {
         get { return _activeCamera; }
@@ -73,52 +73,46 @@ public class CameraController : MonoBehaviour
     public void RotateCamera(Vector3 lookInput, bool moveRigidbody)
     {
         Vector3 cameraRotation = _cameraTarget.transform.localEulerAngles;
+        float yRotation = 0,
+            xRotation = 0;
         if (moveRigidbody)
         {
-            cameraRotation.x = _player.Rigidbody.transform.localEulerAngles.x;
+            cameraRotation.y = transform.localEulerAngles.y;
         }
-        float minAngle = _minAngle;
-        float maxAngle = _maxAngle;
         if (Math.Abs(lookInput.y) > 0.5)
         {
-            float amountToRotate = -lookInput.y * _cameraRotationSpeed * Time.deltaTime;
-            cameraRotation.x += amountToRotate;
+            yRotation = -lookInput.y * _cameraRotationSpeed * Time.deltaTime;
+            cameraRotation.x += yRotation;
         }
 
         if (Math.Abs(lookInput.x) > 0.5)
         {
-            float amountToRotate = lookInput.x * _cameraRotationSpeed * Time.deltaTime;
-            cameraRotation.y += amountToRotate;
+            xRotation = lookInput.x * _cameraRotationSpeed * Time.deltaTime;
+            cameraRotation.y += xRotation;
         }
 
-        //Fix camera angle if broken
-        //float cameraAngle = _player.CameraTarget.transform.localEulerAngles.x;
-        //if (cameraAngle > minAngle && cameraAngle < maxAngle)
-        //{
-        //    float amountToRotate = minAngle - cameraAngle;
-        //    _player.CameraTarget.transform.Rotate(
-        //        new Vector3(amountToRotate, amountToRotate, 0f),
-        //        Space.Self
-        //    );
-        //}
-
-        if (cameraRotation.y >= maxAngle)
+        //TODO:: Limits could be improved
+        if (cameraRotation.x > 180)
         {
-            cameraRotation.y = maxAngle;
+            cameraRotation.x -= 360;
         }
-        if (cameraRotation.y <= minAngle)
-        {
-            cameraRotation.y = minAngle;
-        }
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, _minAngle, _maxAngle);
         if (moveRigidbody)
         {
-            _player.Rigidbody.transform.localEulerAngles = new Vector3(0f, cameraRotation.y, 0f);
-            _cameraTarget.transform.localEulerAngles = new Vector3(cameraRotation.x, 0f, 0f);
+            transform.localEulerAngles = new Vector3(0f, cameraRotation.y, 0f);
+            //_player.Rigidbody.transform.Rotate(new Vector3(0f, xRotation, 0f), Space.Self);
+            _cameraTarget.transform.localEulerAngles = new Vector3(
+                cameraRotation.x,
+                _cameraTarget.transform.localEulerAngles.y,
+                _cameraTarget.transform.localEulerAngles.z
+            );
         }
         else
         {
             _cameraTarget.transform.localEulerAngles = cameraRotation;
         }
+
+        //Fix broken camera
     }
 
     public void ResetCameraRotation()
